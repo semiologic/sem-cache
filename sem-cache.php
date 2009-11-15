504d85b181ed3f1f309086e5d4c362f4f4dcb848
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-11-15 06:56:34 +0000

    works fine
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/sem-cache/trunk@3353 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-cache.php
+++ sem-cache.php
@@ -3,7 +3,7 @@
 Plugin Name: Semiologic Cache
 Plugin URI: http://www.semiologic.com/software/sem-cache/
 Description: An advanced caching module for WordPress.
-Version: 1.2.1 beta
+Version: 1.2.1
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 Text Domain: sem-cache
