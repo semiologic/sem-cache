ed96b65decd12b93d60ca9747851ad9dc3419149
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-12-31 20:17:17 +0000

    cruft
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/sem-cache/trunk@3825 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-cache-base.php
+++ sem-cache-base.php
@@ -1,13 +1,3 @@
 <?php
-$known_headers = array("Last-Modified", "Expires", "Content-Type", "Content-type", "X-Pingback", "ETag", "Cache-Control", "Pragma");
-
-if (!class_exists('CacheMeta')) {
-	class CacheMeta {
-		var $dynamic = false;
-		var $headers = array();
-		var $uri = '';
-		var $post = 0;
-	}
-}
-
+# obsolete file, kept around in case of upgrade problems
 ?>
