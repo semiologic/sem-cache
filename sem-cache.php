72aad989465839f178c566ea3115738552c4e546
Mike Koepke <mike.koepke@gmail.com>
2014-02-05 13:32:27 -0600

    Mark as released

--- sem-cache.php
+++ sem-cache.php
@@ -3,7 +3,7 @@
 Plugin Name: Semiologic Cache
 Plugin URI: http://www.semiologic.com/software/sem-cache/
 Description: An advanced caching module for WordPress.
-Version: 2.6 dev
+Version: 2.6
 Author: Denis de Bernardy & Mike Koepke
 Author URI: http://www.getsemiologic.com
 Text Domain: sem-cache
